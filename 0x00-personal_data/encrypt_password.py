6578aed5dd552b056bff0221cd63d59c1d858603
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-12 14:30:37 +0200

    Adding the code for 6, which is to validate that the password is encrypted

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode("UTF-8"), salt)
+
+
+def is_valid(hash_password: bytes, password: str,) -> bool:
+    """Validates that a provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hash_password)
