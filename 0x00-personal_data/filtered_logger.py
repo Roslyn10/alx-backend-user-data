c0ddc644334338647b7169d07b5b3bffd198a191
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-12 22:11:44 +0200

    Adding the main code for 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,16 +61,34 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connect:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Connects a secure database to read a users table
     """
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD') or " "
-    db = os.getenv('PERSONAL_DATA_DB_NAME')
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
     conn = mysql.connector.connect(user=user,
+                                   password=passwd,
                                    host=host,
-                                   password=password,
-                                   database=db)
+                                   database=db_name)
     return conn
+
+
+def main():
+    """Main point of the code"""
+    db = get_db()
+    logger = get_logger
+    cursor = db.cursor
+    cursor.execute("SELECT * FROM users;")
+    field = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
