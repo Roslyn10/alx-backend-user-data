2f516e65defbdae3e8bd97464129734f171a28b2
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-12 14:32:37 +0200

    Fixing the error in the declaration of the function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,6 +11,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("UTF-8"), salt)
 
 
-def is_valid(hash_password: bytes, password: str,) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates that a provided password matches the hashed password."""
-    return bcrypt.checkpw(password.encode(), hash_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
