fd4591afc9434d14dbcbdc2996dc8bff5651fd81
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-17 11:37:48 +0200

    Fixing the documentation of the codes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,24 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """A class that checks authentication """
+    """A class to handle authentication process
+
+    Provides methods to determine if a request requires authentication,
+    extract the authorization header from an HTTP request,
+    and retrieve the current user
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Determines if a path requires authentication"""
+        """
+        Determines if a specific path requires authentication
+
+        Args:
+            path (str): The path of the request to check for authentication.
+            excluded_paths (List[str]): A list of paths that do not require authentication.
+
+        Return:
+            bool: True if the path requires authentication, False if it is in excluded paths
+        """
         if path is None:
             return True
         elif excluded_paths is None or excluded_paths == []:
@@ -20,7 +34,15 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Returns the authorization header from the request """
+        """
+        Retrieves the 'Authorization' header from an HTTP request
+
+        Args:
+            request (Optional): The HTTP request object that may contain the 'Authorization' header
+
+        Returns:
+            str: The value of the 'Authorization' header if it exists, otherwise None.
+        """
         if request is None:
             return None
         header = request.headers.get('Authorization')
@@ -29,5 +51,13 @@ class Auth:
         return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Returns the current user based on the request"""
+        """
+        Retrieves the current user based on the request
+
+        Args:
+            request (Optional): The HTTP request object used to determine the current user.
+
+        Return:
+            TypeVar('User'): The current user instance if identified, otherwise None.
+        """
         return None
