f9b90b223f7d52f3a18ecb4ce759e5bb0cd61f7c
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-17 15:20:07 +0200

    Adding the code that decodes the basic authoriazation into base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,3 +28,27 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """
+        Returns the decoded value of a Base64 string base64_authorization_header
+
+        Args:
+            base64_authorization_header (str): The Base64 encoded string
+
+        Return:
+            str: The decoded value as a UTF-8 string if valid,
+            otherwise None.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header) is None:
+            return None
+        try:
+            base64_bytes = base64.b64decode(base64_authorization_header, validate=True)
+            decoded_str = base64_bytes.decode('utf-8')
+            return decoded_str
+        except Exception
+            return None
+
