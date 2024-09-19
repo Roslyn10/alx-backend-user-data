974d2c0ddd77668554d95296956c3cc492370d1c
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-19 14:44:52 +0200

    Fixing the syntax and indentation

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,9 +6,11 @@ from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """
+    POST /auth_session/login: Creates a session for a user
     """
     email = request.form.get('email')
     if not email:
@@ -16,24 +18,24 @@ def login():
 
     password = request.form.get('password')
     if not password:
-        return jsonify({"error": "password mising"})
+        return jsonify({"error": "password missing"}), 400
 
     try:
         users = User.search({'email': email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in found_users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
 
-        from api.v1.app import auth
-        
-        user = found_users[0]
-        session_id = auth.create_session(user.id)
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
 
-        SESSION_NAME = getenv("SESSION_NAME")
-        
-        response = jsonify(user.to_json())
-        response.set_cookie(SESSION_NAME, session_id)
+            session_id = auth.create_session(user.id)
 
-        return response
+            SESSION_NAME = getenv("SESSION_NAME")
+
+            response = jsonify(user.to_json())
+            response.set_cookie(SESSION_NAME, session_id)
+
+            return response
+
+    return jsonify({"error": "wrong password"}), 401
