abd3ae88201bd56290b71f2bfd4e9cec5fbdd5eb
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-20 17:04:09 +0200

    Adding the code for user session

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A Model and userSession that inherits from Base"""
+
+from models.base import Base
+
+class UserSession(Base):
+    """A user session that inherits from base"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """
+        Initializes the user class
+
+        Args:
+            *args: Variable length argument list
+            **kwargs: Keyword arguments 
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id', None)
+        self.session_id = kwards.get('session_id', None)
+
