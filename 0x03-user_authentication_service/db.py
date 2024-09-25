d18419dba41f39e127c01a24ea4fb8d58bb4ea58
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-25 12:19:54 +0200

    Adding the rollback session and removing the SQL logs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -44,9 +44,13 @@ class DB:
         Returns:
             User: The newly created user object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
 
     def find_user_by(self, **kwargs):
